4cd36bfbd64797fb00eec6cc52ef5a7fef0ceed3
Pav T <pav@lawn-143-215-62-105.lawn.gatech.edu>
2014-01-14 18:34:57 -0500

    Can add ingredients to database now. Still working on getting google images

--- fridgyThing/fridge/views.py
+++ fridgyThing/fridge/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from fridge.models import Ingredient 
 # Create your views here.
+
+#------------Pav-----------------
 def index(request):
 	return render(request, 'fridge/index.html')
 def app(request):
 	ingredients = Ingredient.objects.all() 
 	return render(request, 'fridge/layout.html', {'ingredients':ingredients} )
-def processing(request):
-	#do something
-	return HttpResponse(status=204) #pass back to the page without changing it
+def addIngredient(request):
+	try:
+		IngName = request.POST['IngName']
+		# IngAmount = float(request.POST['IngAmount'])
+		i = Ingredient(name=IngName,pic='search')
+		i.save();
+	except:
+		#nothing
+		i=1
+	else:
+		return HttpResponseRedirect(reverse('fridge:appPage',args=()))
+
+#-------------------------------
