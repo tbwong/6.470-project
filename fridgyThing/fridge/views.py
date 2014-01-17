cd576443ac9f4d9adedef11175153ca3a488ea59
rujia <rujiazha@gmail.com>
2014-01-16 23:18:54 -0500

    commented out def in views.py

--- fridgyThing/fridge/views.py
+++ fridgyThing/fridge/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
-<<<<<<< HEAD
 from fridge.models import Ingredient, ShoppingList
-=======
-from fridge.models import Ingredient, Pictures
->>>>>>> 0b922416a4a99b10a7d237a93c838dee29f85ac3
 # Create your views here.
 
 #----------------Pav-----------------\/
@@ -52,10 +48,8 @@ def showGraphsPage(request):
 #----------------Jacqui-----------------\/
 
 def showScrapbookPage(request):
-       pictures = [x.picture for x in Pictures.objects.all()]
-       date = [x.date for x in Pictures.objects.all()]
-       caption = [x.caption for x in Pictures.objects.all()]
-       return render(request, 'scrapbook/scrapbook.html', {'pictures':pictures, 'date':date, 'caption':caption})
+       items = 0
+       return render(request, 'scrapbook/scrapbook.html', {'item':items})
 
 #----------------Jacqui-----------------/\
 
@@ -68,16 +62,16 @@ def showShoppingPage(request):
         return render(request, 'shopping/shopping.html', {'itemslist':itemslist, 'memolist':memolist})
 
 
-def addItem(request):
-	try:
-		ItemName = request.POST['ItemName']
-		ItemName.strip()
-		# IngAmount = float(request.POST['IngAmount']) what.
-		i = ShoppingList(item=ItemName,note='')
-		i.save();
-	except:
-		#nothing
-		i=1
-	else:
-		return HttpResponseRedirect(reverse('fridge:appPage',args=()))
+##def addItem(request):
+##	try:
+##		ItemName = request.POST['ItemName']
+##		ItemName.strip()
+##		# IngAmount = float(request.POST['IngAmount']) what.
+##		i = ShoppingList(item=ItemName,note='')
+##		i.save();
+##	except:
+##		#nothing
+##		i=1
+##	else:
+##		return HttpResponseRedirect(reverse('fridge:appPage',args=()))
 #----------------Rujia-----------------/\
