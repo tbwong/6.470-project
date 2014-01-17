0d7a03a11da63b9d17636eccfc51ed198b0d2f54
Jacqui De Sa <jdesa@mit.edu>
2014-01-17 03:25:26 -0500

    Updated views, forms, models, and scrapbook html to fix load errors

---
+++ fridgyThing/fridge/views.py.BASE.2158.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from fridge.models import Ingredient, Pictures
+# Create your views here.
+
+#----------------Pav-----------------\/
+def index(request):
+	return render(request, 'fridge/index.html')
+def showFridge(request):
+	ingredients = Ingredient.objects.all() 
+	return render(request, 'fridge/layout.html', {'ingredients':ingredients} )
+
+
+def addIngredient(request):
+	try:
+		IngName = request.POST['IngName']
+		IngName.strip()
+		# IngAmount = float(request.POST['IngAmount'])
+		i = Ingredient(name=IngName,pic='search')
+		i.save();
+	except:
+		#nothing
+		i=1
+	else:
+		return HttpResponseRedirect(reverse('fridge:appPage',args=()))
+
+#----------------Pav-----------------/\
+#----------------Tiff-----------------\/
+def showGraphsPage(request):
+	#calories,carbs,fat,protein,sodium,sugar
+	calories = [100,20,30,40]
+	carbValues = [100,20,30,40]
+	fatValues = [20,40,50]
+	proteinValues = [100,20,30,40]
+	sodiumValues = [100,20,30,40]
+	sugarValues = [100,20,30,40]
+	currentDates = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+	return render(request, 'graphs/graphs.html',{'cal':calories,
+												'carbs': carbValues,
+												'fat':fatValues,
+												'protein': proteinValues,
+												'sodium': sodiumValues,
+												'sugar': sugarValues,
+												'dates': currentDates
+												})
+#----------------Tiff-----------------/\
+#----------------Jacqui-----------------\/
+
+def showScrapbookPage(request):
+       pictures = [x.picture for x in Pictures.objects.all()]
+       date = [x.date for x in Pictures.objects.all()]
+       caption = [x.caption for x in Pictures.objects.all()]
+       return render(request, 'scrapbook/scrapbook.html', {'pictures':pictures, 'date':date, 'caption':caption})
+
+#----------------Jacqui-----------------/\
+
+
+
+#----------------Rujia-----------------\/
+def showShoppingPage(request):
+        items = 0
+        return render(request, 'shopping/shopping.html', {'item':items})
+#----------------Rujia-----------------/\
